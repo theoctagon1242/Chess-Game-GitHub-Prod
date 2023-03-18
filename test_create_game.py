7172615db309bbd46f7fd0be39c074dba0763d1f
reecechallinor@gmail.com <reecechallinor@gmail.com>
2023-03-18 18:12:51 -0500

    updated test create game

--- test_create_game.py
+++ test_create_game.py
@@ -1,24 +1,32 @@
-#this file is unti tests for create_game.py
 import unittest
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 import create_game
 import chess
 
+# ...
+
 class TestCreateGame(unittest.TestCase):
     
-    def test_create_game_state(self):
-        create_game.dynamodb = MagicMock()
-        table_mock = create_game.dynamodb.Table.return_value
+    # ...
+
+    @patch('create_game.create_game_state')
+    @patch('create_game.send_sms')
+    @patch('create_game.render_board')
+    def test_lambda_handler(self, render_board_mock, send_sms_mock, create_game_state_mock):
+        test_game_id = "test-uuid"
+        test_phone_number = "+1234567890"
+        test_event = {'phone_number': test_phone_number}
+        test_initial_board = "test_initial_board"
+        create_game_state_mock.return_value = test_game_id
+        render_board_mock.return_value = test_initial_board
         
-        game_id = create_game.create_game_state()
+        response = create_game.lambda_handler(test_event, None)
         
-        self.assertIsNotNone(game_id, "The returned game ID should not be None")
-        self.assertTrue(create_game.dynamodb.Table.called, "The Table method should be called")
-        self.assertTrue(table_mock.put_item.called, "The put_item method should be called")
-        
-        put_item_args = table_mock.put_item.call_args[1]['Item']
-        self.assertEqual(put_item_args['status'], 'in_progress', "The initial game status should be 'in_progress'")
-        self.assertEqual(put_item_args['fen'], chess.Board().fen(), "The initial FEN should match the default chess board FEN")
+        create_game_state_mock.assert_called_once()
+        render_board_mock.assert_called_once_with(chess.Board().fen())
+        send_sms_mock.assert_called_once_with(test_phone_number, f"Your game ID is {test_game_id}\n{test_initial_board}")
+        self.assertEqual(response['game_id'], test_game_id, "The response game ID should match the test game ID")
+        self.assertEqual(response['initial_board'], test_initial_board, "The response initial board should match the test initial board")
 
 if __name__ == '__main__':
     unittest.main()
